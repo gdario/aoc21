18e94fba2fa70eb6bba8ac6ec89fedc87bc9b78b
Giovanni d'Ario <giovanni.dario@gmail.com>
2023-12-15 20:18:26 +0100

    The __add__ method is still incomplete

--- day18/day18.py
+++ day18/day18.py
@@ -1,3 +1,6 @@
+import math
+
+
 class Number:
     def __init__(self, x: list) -> None:
         self.x = x
@@ -10,6 +13,14 @@ class Number:
                 self.flatten(item, level+1)
             else:
                 self.xflat.append((item, level))
+
+    def is_not_split(self):
+        res = any([x[0] >= 10 for x in self.xflat])
+        return res
+
+    def is_not_exploded(self):
+        res = any([x[1] == 4 for x in self.xflat])
+        return res
 
     def explode(self):
         for k, item in enumerate(self.xflat):
@@ -25,3 +36,24 @@ class Number:
                 self.xflat.pop(k)
                 self.xflat.insert(k, (0, 3))
                 break
+
+    def split(self):
+        for k, item in enumerate(self.xflat):
+            if item[0] >= 10:
+                left, right = math.floor(item[0] / 2), math.ceil(item[0] / 2)
+                self.xflat[k] = (left, item[1] + 1)
+                self.xflat.insert(k + 1, (right, item[1] + 1))
+                break
+
+    def reduce(self):
+        while self.is_not_split or self.is_not_exploded:
+            if self.is_not_split():
+                self.split()
+            else:
+                self.explode()
+
+    def __add__(self, other):
+        x = [self.x, other.x]
+        res = Number(x)
+        # res.reduce()
+        return res
